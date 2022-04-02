0da283b3d0c507b07081261d0a58f7c6c0c32343
Phillip Schichtel <phillip@schich.tel>
2022-04-02 21:57:49 +0200

    initial script

---
+++ docker-forward.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# Docker Forward
+
+set -euo pipefail
+
+metadata() {
+    local socat
+    socat="$(which socat 2>/dev/null || echo "")"
+    local socat_notice=""
+    if [ -z "$socat" ]
+    then
+        socat_notice=" (socat currently missing!)"
+    fi
+    cat <<CUT
+{
+    "SchemaVersion": "0.1.0",
+    "Vendor": "Phillip Schichtel",
+    "Version": "v1",
+    "ShortDescription": "Docker Port Forward${socat_notice}"
+}
+CUT
+}
+
+usage() {
+    cat <<EOF
+Usage: docker port-forward <local address> <local port> <remote address> <remote port> [remote docker network]
+EOF
+}
+
+if [ "${1:-}" == "docker-cli-plugin-metadata" ]
+then
+    metadata
+    exit 0
+fi
+
+shift
+if [ "$#" = 0 ]
+then
+    usage
+    exit 0
+fi
+
+env
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+    usage
+    exit 0
+fi
+
+if [ "$#" != 4 ]
+then
+    echo "Missing argument!"
+    usage
+    exit 1
+fi
+
+local_addr="${1?no local address}"
+local_port="${2?no local port}"
+target_host="${3?no target host}"
+target_port="${4?no target port}"
+target_network="${5:-}"
+proxy_container_image="${PROXY_IMAGE:-"pschichtel/deployment-helper:1-alpine"}"
+
+if [ -n "$target_network" ]
+then
+    target_network="--net='${target_network}'"
+fi
+
+docker_command="${DOCKER_CLI_PLUGIN_ORIGINAL_CLI_COMMAND:-"docker"}"
+
+command="'$docker_command' run --rm -i ${target_network} '$proxy_container_image' socat - 'TCP-CONNECT:${target_host}:${target_port}'"
+socat -d -d -ls "TCP-LISTEN:${local_port},bind=${local_addr},fork,reuseaddr" "EXEC:${command}"
